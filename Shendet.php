bafef4bcfb0467ed08a02b9bedef5e1c050ff0cb
LirjetaKastrati <73938573+LirjetaKastrati@users.noreply.github.com>
2021-05-14 23:45:11 +0300

    Create Shendet.php

---
+++ Shendet.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="shortcut icon"  type="image/x-icon" href="img/favicon.ico" />
+  <meta charset="utf-8">
+  <title>Shëndet</title>
+  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="style/responsive.css">
+  <link rel="stylesheet" href="style/audio.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600;700&display=swap"> 
+	  <script type="text/javascript">
+var now = new Date();
+var hours = now.getHours();
+//dita
+if (hours > 6 && hours < 19){
+ document.write ('<body style="background-color: #f0cef2">');
+}
+//nata
+else {
+ document.write ('<body style="background-color: #f0e5c9">');
+}
+
+</script>
+</head>
+<body>
+	<div id="wrapper" class="wrapper">
+		<header id="head">
+			<div class="wrapper1">
+				<div class="logo">
+					<a href="#">Shëndet</a>
+				</div>
+			
+				    <div class="search">
+					<form action="post">
+						<input type="text" placeholder="Kërko në faqe">
+						<button type="submit"><i class="fas fa-search"></i></button>
+					</form>
+				    </div>
+				    <br><br><br>
+					<nav>
+		 			     <ul class="navigation">
+				  		 <li><a href="ballina.html">BALLINA</a></li>
+				  		 <li><a href="#">FULL WIDTH</a></li>
+							
+						 <li id="dropdowni"><a href="#">KATEGORITË</a>
+							<ul>    
+								<li><a href="mode.html">Modë</a></li>
+								<li><a href="shendet.html">Shëndet</a></li>
+								<li><a href="hobi.html">Hobi</a></li>
+								<li><a href="storjet.html">Storje</a></li>
+							</ul>
+						 </li>
+
+						 <li><a href="loja.html">LOJA</a></li>
+						<li><a href="shendet.html">GALERIA</a></li>
+						
+						<li id="dropdowni"><a href="#">KYÇU/REGJISTROHU</a>
+							<ul>    
+								<li><a href="dragdrop.html">Kyçu</a></li>
+								<li><a href="regjistrimi.html">Regjistrohu</a></li>
+								<li><a href="contact.html">Na kontakto</a></li>
+							</ul>
+					    </li>						
+				  	     </ul>
+	      				</nav>
+				</div>	
+		 	</header>
+			
+		        			
+	         <main>
+			  		 
+								
+					
+				<section style="padding-top: 140px;" id="facilis">
+					<div class="facilis_title">
+						<h2 style="padding-top: 20px">Shumëçka nga stili i jetesës së shëndetshme </h2>
+						<div class="facilis_border"></div>
+					</div>
+						     
+					<div class="facilis_content"> 
+					     <ul>
+					        <li>
+								<div class="single_facilis">
+									<img src="img/natyre.jpg" width="300" height="170" alt="img1">
+									<h4>ECJE NË NATYRË</h4>
+									<p><a href="https://www.kap-ks.com/ecja-dhe-shendeti/" target="_blank">
+										Këshilla për ecje nga Klubi Alpin Prishtina</a></p>
+									<p><a href="https://www.keshilla.net/shume-te-mira-nga-ecja-ne-mengjes/" target="_blank">
+									    Shumë të mira nga ecja në mëngjes</a></p>
+									<p><a href="https://klankosova.tv/perfitimet-e-medha-shendetesore-te-ecjes/" target="_blank">
+										Përfitimet e mëdha shëndetësore të ecjes</a></p>
+						   		</div>
+					       </li>
+						     
+				            <li>
+								<div class="single_facilis">
+						  			<img src="img/ushqime.jpg" width="300" height="170" alt="img2">
+						  			<h4>USHQIME</h4>
+						  			<p><a href="https://www.botasot.info/shendetesia/626463/10-ushqimet-me-te-shendetshme-per-njeriun/" target="_blank">
+										10 ushqimet më të shëndetshme</a></p>
+								    <p><a href="https://www.balkanweb.com/ushqimet-kunder-kancerit-qe-duhet-te-konsumohen-rregullisht/" target="_blank">
+										Ushqimet kundër kancerit që duhet të konsumohen</a></p>
+								    <p><a href="https://www.renatomekolli.com/">
+                                        Receta tradicionale shqipëtare nga Renato Mekolli</a></p>
+					   			</div>
+							</li>
+						     
+							<li>
+					   			<div class="single_facilis" style="padding-top: 1px;">
+					     			<h4>MË SHUMË NGA LAJME</h4>
+									 <p><a href="https://www.kap-ks.com/ecja-dhe-shendeti/" target="_blank">
+										Këshilla për ecje nga Klubi Alpin Prishtina</a></p>
+									<p><a href="https://www.keshilla.net/shume-te-mira-nga-ecja-ne-mengjes/" target="_blank">
+									    Shumë të mira nga ecja në mëngjes</a></p>
+									<p><a href="https://klankosova.tv/perfitimet-e-medha-shendetesore-te-ecjes/" target="_blank">
+										Përfitimet e mëdha shëndetësore të ecjes</a></p>
+									<p><a href="https://www.botasot.info/shendetesia/626463/10-ushqimet-me-te-shendetshme-per-njeriun/" target="_blank">
+											10 ushqimet më të shëndetshme</a></p>
+									<p><a href="https://www.balkanweb.com/ushqimet-kunder-kancerit-qe-duhet-te-konsumohen-rregullisht/" target="_blank">
+											Ushqimet kundër kancerit që duhet të konsumohen</a></p>
+									<p><a href="https://www.renatomekolli.com/">
+											Receta tradicionale shqipëtare nga Renato Mekolli</a></p>
+                                        
+								
+					    		</div>
+							</li>
+			
+			
+							<li>
+					   			<div class="single_facilis">
+									<img src="img/fitnesjogapilates.jpg" width="300" height="170" alt="img4">
+									<h4>FITNES, JOGA DHE PILATES</h4>	
+									<p><a href="https://www.sportvision.al/revista/stervitje/827-fakte-interesante-nga-bota-e-fitnesit" target="_blank">
+										Disa nga fakte interesante nga bota e fitnesit</a></p>
+									<p><a href="https://klankosova.tv/joga-dhe-perfitimet-e-saj/" target="_blank">
+										Joga dhe përfitimet e saj</a></p>
+									<p><a href="https://www.dietaime.com/2018/06/27/a-e-dini-sa-kalori-digjen-gjate-15-minutave-qe-ju-ushtroni/" target="_blank">
+										Ushtrimet pilates dhe luftimi i sëmundjeve</a></p>
+				       			</div>
+				  			</li>
+						   
+							<li>
+				   				<div class="single_facilis">
+									<img src="img/sport.jpg" width="300" height="170" alt="img5">
+									<h4>SPORT</h4>
+									<p><a href="https://manshealthblog.wordpress.com/2016/11/20/si-ndikon-sporti-ne-shendetin-tone/" target="_blank">
+										Si ndikojnë sportet e ndryshme në shëndetin tonë</a></p>
+									<p><a href="https://fakteplus.net/10-rekordet-me-te-pathyeshme-ne-historine-e-futbollit/" target="_blank">
+										10 rekorde interesante në historinë e futbollit botëror</a></p>
+									<p><a href="https://scrubbing.in/much-running-bad-heart/#:~:text=But%20endurance%20running%20has%20actually,men%20of%20the%20same%20age." target="_blank">
+										Si ndikon vrapimi në shëndet?</a></p>
+				    			</div>
+							</li>
+						   
+							<li>
+								<div class="single_facilis" style="padding-top: 8px;">
+									<h4>MË SHUMË LAJME</h4>
+									<p><a href="https://www.sportvision.al/revista/stervitje/827-fakte-interesante-nga-bota-e-fitnesit" target="_blank">
+										Disa nga fakte interesante nga bota e fitnesit</a></p>
+									<p><a href="https://klankosova.tv/joga-dhe-perfitimet-e-saj/" target="_blank">
+										Joga dhe përfitimet e saj</a></p>
+									<p><a href="https://www.dietaime.com/2018/06/27/a-e-dini-sa-kalori-digjen-gjate-15-minutave-qe-ju-ushtroni/" target="_blank">
+										Ushtrimet pilates dhe luftimi i sëmundjeve</a></p>
+									<p><a href="https://manshealthblog.wordpress.com/2016/11/20/si-ndikon-sporti-ne-shendetin-tone/" target="_blank">
+											Si ndikojnë sportet e ndryshme në shëndetin tonë</a></p>
+									<p><a href="https://fakteplus.net/10-rekordet-me-te-pathyeshme-ne-historine-e-futbollit/" target="_blank">
+											10 rekorde interesante në historinë e futbollit botëror</a></p>
+									<p><a href="https://scrubbing.in/much-running-bad-heart/#:~:text=But%20endurance%20running%20has%20actually,men%20of%20the%20same%20age." target="_blank">
+											Si ndikon vrapimi në shëndet?</a></p>
+									
+								</div>
+							</li>
+						</ul>
+					</div>
+				 </section>
+				 
+					
+				 <section class="audiot">
+
+					<section style="padding-top: 50px; padding-bottom: 0px; font-size: large; width: 98%;">
+						<div class="facilis_title">
+							<h2>MUZIKË PËR AKTIVITETET TUAJA FIZIKE</h2>
+						</div>
+					</section>
+
+						<table border="1" cellspacing="1.3px">
+							<thead>
+								<tr>
+									<th>Zhanri</th>
+									<th>Autori</th>
+									<th>Titulli</th>
+									<th>Kliko &#9654;  për të dëgjuar</th>									
+								</tr>
+							</thead>
+		
+							<tbody>
+								<tr>
+									<td>Yoga</td>
+									<td>Eden Piano</td>
+									<td>Songs Of Eden / Bali - 2020</td>
+									<td>
+										<audio controls loop>
+											<source src="audio/eden-bali.mp3"
+												type="audio/mpeg">
+											Kjo audio nuk përkrahet nga shfletuesi juaj
+											<a href="https://www.youtube.com/watch?v=tO2z-Y5Xv-8">. Kliko linkun për të dëgjuar</a>
+										</audio>
+									</td>
+								</tr>
+
+								<tr>
+									<td>Pilates</td>
+									<td>Fly : Pilates</td>
+									<td>Awakening all cells - 2013</td>
+									<td>
+										<audio controls loop>
+											<source src="audio/fly-pilates.mp3"
+												type="audio/mpeg">
+											Kjo audio nuk përkrahet nga shfletuesi juaj
+											<a href="https://www.youtube.com/watch?v=hYahOGi4rVw">. Kliko linkun për të dëgjuar</a>
+										</audio>
+									</td>
+								</tr>
+
+								<tr>
+									<td>Meditim</td>
+									<td>Meditate Youtube</td>
+									<td>Relaxing music / Meditation music</td>
+									<td>
+										<audio controls loop>
+											<source src="audio/meditation.mp3"
+												type="audio/mpeg">
+											Kjo audio nuk përkrahet nga shfletuesi juaj
+											<a href="https://www.youtube.com/watch?v=pDNaSCqW8qo">. Kliko linkun për të dëgjuar</a>
+										</audio>
+									</td>
+								</tr>
+
+								<tr>
+									<td>Relaks</td>
+									<td>BodyMindPower Youtube</td>
+									<td>Music for Yoga, Relaxation, Meditation & Sleep</td>
+									<td>
+										<audio controls loop>
+											<source src="audio/relaxation.mp3"
+												type="audio/mpeg">
+											Kjo audio nuk përkrahet nga shfletuesi juaj
+											<a href="https://www.youtube.com/watch?v=cyXKslqkj5I">. Kliko linkun për të dëgjuar</a>
+										</audio>
+									</td>
+								</tr>
+
+								<tr>
+									<td>Yoga</td>
+									<td>Katie 2004</td>
+									<td>Yoga and sea - 2015</td>
+									<td>
+										<audio controls loop>
+											<source src="audio/yoga-sea.mp3"
+												type="audio/mpeg">
+											Kjo audio nuk përkrahet nga shfletuesi juaj
+											<a href="https://www.youtube.com/watch?v=cyXKslqkj5I">. Kliko linkun për të dëgjuar</a>
+										</audio>
+									</td>
+								</tr>
+		
+								<tr>
+									<td>Vrapim</td>
+									<td>Sander van Doorn</td>
+									<td>Spotlight</td>
+									<td>
+										<audio controls loop>
+											<source src="audio/running.mp3"
+												type="audio/mpeg">
+											Kjo audio nuk përkrahet nga shfletuesi juaj
+											<a href="https://www.youtube.com/watch?v=MqF3Z6izwLI">. Kliko linkun për të dëgjuar</a>
+										</audio>
+									</td>
+								</tr>
+
+							</tbody>
+							<tfoot>
+								<tr>
+									<th style="padding-bottom: 15px;" align="center" colspan="4">
+									<p>Na kontaktoni për të propozuar muzikën që juve ju pëlqen! </p>
+									<a id="contact2" href="contact.html">NA KONTAKTONI</a>
+									</th>
+
+
+								</tr>
+							</tfoot>
+						</table>
+					</section>
+
+
+			
+			<section id="footer_ContactUs">
+				<ul>
+					<li>
+						<div class="f_ContactUs">
+						    <h4>Na kontaktoni</h4>	
+							<address>
+						   
+						   Revista Lifestyle  <br>
+						   Rr. "Eqrem Çabej"<br>
+						   Prishtinë, Kosovë <br>
+						   10000 <br>
+						   Tel: +383 44 000 000 <br>
+						   Fax: +383 38 000 000 <br>
+						   E-mail: revistalifestyleks@gmail.com <br>
+						   
+							 </address>
+						   </div>
+					   </li>
+						   <li>
+					   <div class="f_ContactUs">
+						   <h4>Vegëza </h4>
+						   <ul>
+							   <li><a href="https://www.elle.com/">Revista Elle</a></li>
+							   <li><a href="http://lifestylemagazineonline.com/">Revista Lifestyle US</a></li> 
+							   <li><a href="https://www.theguardian.com/us/lifeandstyle">The Guardian / Lifestyle</a></li>
+							   <li><a href="https://www.lifestylesports.com/eu/">Lifestyle Sports</a></li>
+						   </ul>
+						   </div>
+					   </li>
+				   
+					   <li>
+						  <div class="f_ContactUs">
+						   <h4>Nga faqja </h4>
+						   <h3>Menaxhimi i faqes</h3>
+						   <p>
+							  Kjo faqe kontrollohet dhe menaxhohet nga stafi i Revistës Lifestyle. <br>
+							  Revista filloi si projekt gjatë studimeve në fakultet.
+							  </p>
+					   
+							   <div class="footer_read">
+							<a href="doc/per_faqen.pdf">Lexo më shumë >></a>
+							   </div>
+							  </div>
+						  </li>
+				   
+						   <li>
+						  <div class="f_ContactUs">
+						   <h4> Porositni revistën e shtypur </h4>
+						   <div class="footer_form">
+							   <form action="">
+								<input type="text" placeholder="Emri">
+							   <input type ="email" placeholder="E-mail-i">
+							   <input id="submitbutton" type="submit" value="SUBMIT">
+							   </form>
+							   </div>
+						   </div>
+					   </li>
+				   </ul>
+			   <ul id="icons" class="social-icons">
+					<li><a href="https://www.instagram.com/revistalifestyleks/"><i id="instagram" class="fab fa-instagram"></i></a></li>
+					<li><a href="https://www.facebook.com/reviste.lifstyl.7/"><i id="facebook" class="fab fa-facebook"></i></a></li>
+					<li><a href="https://twitter.com/RLifestyle_Ks"><i id="twitter" class="fab fa-twitter"></i></a></li>
+					</ul>
+					<br> <br>
+			   </section>
+		   
+		   <footer>
+				<div class="footer_text">
+					<p> Copyright © 2013-All Rights Reserved - Domain Name
+						<span> 
+							Template by OS Templates
+							
+						</span>
+				    </p>
+			    </div>
+			</footer>
+		</main>
+	   </div>
+									   
+	</body>
+</html>
+                                                                                                                 
