d39be2ce3bb55e0e13847e75ee635eb0617db3db
Melisastojkaj1 <73940668+Melisastojkaj1@users.noreply.github.com>
2021-05-16 14:52:19 +0200

    add mode.php

---
+++ mode.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="shortcut icon"  type="image/x-icon" href="img/favicon.ico" />
+  <meta charset="utf-8">
+  <title>Modë</title>
+  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="style/responsive.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600;700&display=swap"> 
+	  <script type="text/javascript">
+var now = new Date();
+var hours = now.getHours();
+//dita
+if (hours > 6 && hours < 19){
+ document.write ('<body style="background-color: #f0cef2">');
+}
+//nata
+else {
+ document.write ('<body style="background-color:#f0e5c9">');
+}
+
+</script>
+</head>
+<body>
+	<div id="wrapper" class="wrapper">
+		<header id="head">
+			<div class="wrapper1">
+				<div class="logo">
+					<a href="#">Modë</a>
+				</div>
+			
+				    <div class="search">
+					<form action="post">
+						<input type="text" placeholder="Kërko në faqe">
+						<button type="submit"><i class="fas fa-search"></i></button>
+					</form>
+				    </div>
+				    <br><br><br>
+					<nav>
+		 			     <ul class="navigation">
+				  		 <li><a href="ballina.html">BALLINA</a></li>
+				  		 <li><a href="#">FULL WIDTH</a></li>
+							
+						<li id="dropdowni"><a href="#">KATEGORITË</a>
+							<ul>    
+								<li><a href="mode.html">Modë</a></li>
+								<li><a href="shendet.html">Shëndet</a></li>
+								<li><a href="hobi.html">Hobi</a></li>
+								<li><a href="storjet.html">Storje</a></li>
+							</ul>
+						</li>
+						   
+						<li><a href="loja.html">LOJA</a></li>
+						<li><a href="gallery.html">GALERIA</a></li>
+						
+						<li id="dropdowni"><a href="#">KYÇU/REGJISTROHU</a>
+							<ul>    
+								<li><a href="dragdrop.html">Kyçu</a></li>
+								<li><a href="regjistrimi.html">Regjistrohu</a></li>
+								<li><a href="contact.html">Na kontakto</a></li>
+							</ul>
+					    </li>						
+				  	     </ul>
+	      				</nav>
+				</div>	
+		 	</header>
+			
+		        			
+	         <main>
+			  		 
+								
+					
+				<section style="padding-top: 140px;" id="facilis">
+					<div class="facilis_title">
+						<h2 style="padding-top: 20px;">Shumëçka nga bota e modës </h2>
+						<div class="facilis_border"></div>
+					</div>
+						     
+					<div class="facilis_content"> 
+					     <ul>
+					        <li>
+								<div class="single_facilis" >
+									<img src="img/trendet2020.jpg" width="300" height="170" alt="img1">
+									<h4>TRENDET 2020</h4>
+									<p><a href="https://www.thetrendspotter.net/fashion-trends-from-spring-summer-2019-fashion-weeks/" target="_blank">
+									   10 stile trendi vjeshtore që dominuan në vitin 2020</a></p>
+									 <p><a href="https://www.cosmopolitan.com/style-beauty/fashion/g30256598/fashion-trends-2020/" target="_blank">
+									     Trende të 2020-tës që do iu përcjellin edhe në 2021-tën</a></p>
+									 <p><a href="https://www.vogue.com/article/fall-2020-biggest-street-style-trends" target="_blank">
+									     8 trendet e veshjeve vjeshtore "Street style"</a></p>
+						   		</div>
+					       </li>
+						     
+				            <li>
+								<div class="single_facilis" >
+						  			<img src="img/trendet2021.jpg" width="300" height="170" alt="img2">
+						  			<h4>TRENDET 2021</h4>
+						  			<p><a href="https://www.harpersbazaar.com/fashion/fashion-week/g34074135/spring-2021-fashion-trends/" target="_blank">
+									   12 trende të koleksioneve për pranverën 2021</a></p>
+								        <p><a href="https://www.net-a-porter.com/en-us/porter/article-844911deeddee838/fashion/art-of-style/ss21-fashion-trends" target="_blank">
+									    Si t'i kombinojmë veshjet e të gjitha stileve në vitin 2021</a>	</p>
+								        <p><a href="https://www.pantone.com/articles/fashion-color-trend-report/new-york-fashion-week-spring-summer-2021">
+									   Ngjyrat e veshjeve që priten të dominojnë në vitin 2021</a></p>
+					   			</div>
+							</li>
+						     
+							<li>
+					   			<div class="single_facilis" >
+									<img src="img/more1.jpg" width="300" height="170" alt="img1">   
+					     			
+					      			<p>- Bob Dylan prinë në listën e 50 artistëve meshkuj më me stil. 
+								  <a href="https://www.esquire.com/style/advice/g799/most-stylish-musicians/" target="_blank">Lista.</a></p>
+							    <!--    <p> - Fustanet janë veshja e dytë më e vjetër. Madje deri ne vitet 1600-1700 edhe burrat edhe gratë i vishnin 
+										ato. 
+									<a href="https://www.boldsky.com/fashion/facts/history-of-skirts-second-oldest-garment-known-to-mankind-088840.html" target="_blank">
+									   Fustanet.</a></p>  -->
+								<p> <abbr title="Bulletproof Boy Scouts"> BTS ngrit standardin e veshjeve për meshkuj,
+									<a href="https://www.esquire.com/entertainment/music/a34654383/bts-members-be-album-interview-2020/" target="_blank"> 
+										shihen në revistën Esquire </a></p>
+								<p> - Evolimi i stilit të veshjes së Melania Trump, 
+									<a href="https://style.nine.com.au/latest/melania-trump-first-lady-style/96c4c57d-58b1-49bf-a7a4-1ced02a5deb5" target="_blank">
+									 sipas revistes Style Nine</a></p>
+								<p> - 40 vjet të Baletit Spandau i shfaqur
+									<a href="https://www.theguardian.com/fashion/gallery/2020/nov/30/true-gold-40-years-of-spandau-ballet-style-in-pictures" target="_blank">në foto.</a></p>
+							
+					    		</div>
+							</li>
+			
+			
+							<li>
+					   			<div class="single_facilis">
+									<img src="img/blogere.jpg" width="300" height="170" alt="img4">
+									<h4>BLOGERE</h4>	
+									<p> <a href="https://kosovalive.org/si-eshte-te-jesh-blogere-ne-prishtine/" target="_blank">
+										Si është të jesh blogere femër në Prishtinë</a> </p>
+									<p><a href="https://firstsiteguide.com/examples-of-blogs/" target="_blank">
+										Blogjet më të famshme në botë në vitin 2020</a></p>
+									<p><a href="https://www.beachtomato.com/fashion/10-fashion-bloggers-you-need-to-be-following-on-instagram/" target="_blank">
+										Blogeret më të famshme në instagram</a></p>
+				       			</div>
+				  			</li>
+						   
+							<li>
+				   				<div class="single_facilis">
+									<img src="img/brende.jpg" width="300" height="170" alt="img5">
+									<h4>BRENDE</h4>
+									<p><a href="https://www.globalbrandsmagazine.com/best-shoe-brands-in-the-world/" target="_blank">
+										Brendet më të famshme të këpucëve në botë</a></p>
+									<p><a href="https://luxe.digital/lifestyle/style/best-jeans-brands-women/" target="_blank">
+										Rankimi i xhinseve më të shitura për femra</a></p>
+									<p><a href="https://www.zara.com/xk/en/" target="_blank">
+										Zara vjen me risi në shitje online</a></p>
+				    			</div>
+							</li>
+						   
+							<li>
+								<div class="single_facilis">
+									<h4 style="padding-top: 30px;">MË SHUMË NGA BRENDET</h4>
+									<p> <a href="https://www.alltopeverything.com/top-10-sportswear-brands/" target="_blank">
+										Brendet më të mëdha të veshjeve sportive </a> </p>
+									<p> <a href="https://lifewithme.com/affordable-designer-bags-label/" target="_blank">
+										Çantat më të lira nga brendet e shtrenjta</a> </p>
+									<p> <a href="https://legitcheck.app/guides/gucci/gucci-belt-fake-vs-real-comparison-guide-gg-belt/" target="_blank">
+										Si dallohet një rrip origjinal i Gucci-t </a> </p>
+									<p> <a href="https://www.facebook.com/liastubllaofficial/videos/vb.121301181255135/3771210046264212/?type=3&theater" target="_blank">
+										Dizajni i Lia Stublla-s për Dina Ragheb </a> </p>
+									<p> <a href="https://www.sarashala.com/collections/new-arrivals" target="_blank"> 
+										Modelet e reja të aksesorëve nga dizajnerja Sara Shala</a></p>
+									<p><a href="https://prive.al/blogerja-kosovare-rita-saraci-tani-edhe-me-koleksion-veshjesh-ne-emrin-e-saj/" target="_blank">
+										Blogerja Rita Saraçi me koleksion personal</a></p>
+								<!--	<p> <a href="https://konica.al/2020/09/blogerja-kosovare-me-nje-te-ardhme-premtuese/" target="_blank">
+										Blogerja Naltina Maloku me të ardhme premtuese</a></p>									
+									<p> <a href="https://www.urbanindustry.co.uk/blogs/news/a-brief-history-of-the-converse-chuck-taylor-all-star" target="_blank"> 
+										Histori e Converse Chuck Taylor All Star </a></p> 
+								-->	
+									
+								</div>
+							</li>
+						</ul>
+					</div>
+	             </section>
+									   
+			
+			<section id="footer_ContactUs">
+				<ul>
+					<li>
+						<div class="f_ContactUs">
+						    <h4>Na kontaktoni</h4>	
+							<address>
+						   
+						   Revista Lifestyle  <br>
+						   Rr. "Eqrem Çabej"<br>
+						   Prishtinë, Kosovë <br>
+						   10000 <br>
+						   Tel: +383 44 000 000 <br>
+						   Fax: +383 38 000 000 <br>
+						   E-mail: revistalifestyleks@gmail.com <br>
+						   
+							</address>
+						   </div>
+					   </li>
+						   <li>
+					   <div class="f_ContactUs">
+						   <h4>Vegëza </h4>
+						   <ul>
+							   <li><a href="https://www.elle.com/">Revista Elle</a></li>
+							   <! -- si të bëjmë contact us shembull tek qekjo më poshtë -->
+							   <li><a href="http://lifestylemagazineonline.com/">Revista Lifestyle US</a></li> 
+							   <li><a href="https://www.theguardian.com/us/lifeandstyle">The Guardian / Lifestyle</a></li>
+							   <li><a href="https://www.lifestylesports.com/eu/">Lifestyle Sports</a></li>
+						   </ul>
+						   </div>
+					   </li>
+				   
+					   <li>
+						  <div class="f_ContactUs">
+						   <h4>Nga faqja </h4>
+						   <h3>Menaxhimi i faqes</h3>
+						   <p>
+							  Kjo faqe kontrollohet dhe menaxhohet nga stafi i Revistës Lifestyle. <br>
+							  Revista filloi si projekt gjatë studimeve në fakultet.
+							  </p>
+					   
+							   <div class="footer_read">
+							<a href="doc/per_faqen.pdf">Lexo më shumë >></a>
+							   </div>
+							  </div>
+						  </li>
+				   
+						   <li>
+						  <div class="f_ContactUs">
+						   <h4> Porositni revistën e shtypur </h4>
+						   <div class="footer_form">
+							   <form action="">
+								<input type="text" placeholder="Emri">
+							   <input type ="email" placeholder="E-mail-i">
+							   <input id="submitbutton" type="submit" value="SUBMIT">
+							   </form>
+							   </div>
+						   </div>
+					   </li>
+				   </ul>
+			   <ul id="icons" class="social-icons">
+					<li><a href="https://www.instagram.com/revistalifestyleks/"><i id="instagram" class="fab fa-instagram"></i></a></li>
+					<li><a href="https://www.facebook.com/reviste.lifstyl.7/"><i id="facebook" class="fab fa-facebook"></i></a></li>
+					<li><a href="https://twitter.com/RLifestyle_Ks"><i id="twitter" class="fab fa-twitter"></i></a></li>
+					</ul>
+					<br> <br>
+			   </section>
+		   
+		   <footer>
+				<div class="footer_text">
+					<p> Copyright © 2013-All Rights Reserved - Domain Name
+						<span> 
+							Template by OS Templates
+						</span>
+				    </p>
+			    </div>
+			</footer>
+		</main>
+	   </div>
+									   
+	</body>
+</html>
