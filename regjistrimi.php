a97c9c3a0b73137bb52577e300cc9166ed59d3e7
LirjetaKastrati <73938573+LirjetaKastrati@users.noreply.github.com>
2021-05-17 00:01:27 +0300

    Update regjistrimi.php

--- regjistrimi.php
+++ regjistrimi.php
@@ -278,14 +278,101 @@ setcookie("Free_cookies",$color,time()-3600);
                 <li>
                <div class="f_ContactUs">
                 <h4> Porositni revistën e shtypur </h4>
-                <div class="footer_form">
-                    <form action="">
-                     <input style="background-color: white;" type="text" placeholder="Emri">
-                    <input type ="email" placeholder="E-mail-i">
-                    <input id="submitbutton" type="submit" value="SUBMIT">
-                    </form>
-                    </div>
-                </div>
+                <?php
+
+  define ("BR","<BR>");
+
+  // define a custom function to sanitize the user input data as a precuation from any malicious attacks
+  function test_input($data) {
+
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+  //initialize variables for value as well as errors
+  $name=$email=$gender=$comment=$website="";
+  $name_err=$email_err=$gender_err=$comment_err=$website_err="";
+
+  // Check if coming from submitted form and do the validations on user inputs in form
+  if ($_SERVER["REQUEST_METHOD"]=="POST") {
+    if (empty($_POST["name"])) {
+      $name_err = "Name cannot be left blank.";
+    }
+    else {
+      $name = test_input($_POST["name"]);
+      // check for correctness of name or validate our name test_input
+      if (!preg_match("/^[a-zA-z ]*$/",$name)) {
+        $name_err = "Name is not in valid format, can contain only letters.";
+      }
+    }
+
+    if (empty($_POST["email"])) {
+      $email_err = "Email cannot be left blank.";
+    }
+    else {
+      $email = test_input($_POST["email"]);
+      if (!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        $email_err = "Email format is not correct.";
+      }
+    }
+    if (!empty($_POST["website"])) {
+      $website = test_input($_POST["website"]);
+
+     if (!filter_var($website,FILTER_VALIDATE_URL)){
+        $website_err = "Website format is not correct.";
+      }
+
+    }
+    if (!empty($_POST["comment"])) {
+      $comment = test_input($_POST["comment"]);
+    }
+    if (!empty($_POST["gender"])) {
+      $gender = test_input($_POST["gender"]);
+    }
+
+  }
+ ?>
+<body>
+  <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST" target="_self">
+
+    Emeri: <input type="text" name="name" id="name" value="<?php echo $name; ?>"> <br>
+    <span style="color:red"> <?php echo $name_err; ?></span><br>
+    Email:<input type = "text" name="email" id="email" value="<?php echo $email; ?>"><br>
+    <span style="color:red"> <?php echo $email_err; ?></span><br>
+    Website:<input type = "text" name="website" id="website" value="<?php echo $website; ?>"><br>
+    <span style="color:red"> <?php echo $website_err; ?></span><br>
+
+    Gjinia:<br>
+    <input type = "radio" name="gender" id="female" value="female" <?php if ($gender=="female") {echo "checked";} ?> >Female
+    <input type = "radio" name="gender" id="male" value="male" <?php if ($gender=="male") {echo "checked";} ?>>Male
+    <input type = "radio" name="gender" id="other" value="other" <?php if ($gender=="other") {echo "checked";} ?>>other<br><br>
+    <span style="color:red"> <?php echo $gender_err; ?></span><br>
+    Coment:
+    <textarea name="comment" id="comment" value="" cols="50" rows="10"><?php echo $comment; ?></textarea> <br><br><br>
+    <span style="color:red"> <?php echo $comment_err; ?></span><br>
+    <input type="Submit" value="Submit" name="Submit">
+
+    
+
+  </form>
+
+  <?php
+
+
+   // if the form is submitted and no errors are found in any of the fields, show the success message with all of the data
+   if (($_SERVER["REQUEST_METHOD"]=="POST")
+        and ($name_err=="" and $email_err=="" and $website_err=="" and $gender_err=="" and $comment_err=="")){
+     echo BR.BR.BR.'Submission Successful: '.BR.BR;
+     echo 'Name: '.$name.BR;
+     echo 'E-mail: '.$email.BR;
+     echo 'Portal: '.$website.BR;
+     echo 'Gender: '.$gender.BR;
+     echo 'Comment: '.$comment.BR;
+
+   }
+
+   ?>
                     <?php
    try {
       $error = 'Mbiemri';
