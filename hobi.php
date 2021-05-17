44e0d551e0e034d771c11b114ba753b20153b8d8
LirjetaKastrati <73938573+LirjetaKastrati@users.noreply.github.com>
2021-05-17 12:12:51 +0300

    Create hobi.php

---
+++ hobi.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="shortcut icon"  type="image/x-icon" href="img/favicon.ico" />
+  <meta charset="utf-8">
+  <title>Hobi</title>
+  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="style/responsive.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600;700&display=swap"> 
+	  <script type="text/javascript">
+var now = new Date();
+var hours = now.getHours();
+//dita
+if (hours > 6 && hours < 19){
+ document.write ('<body style="background-color: #f0cef2">');
+}
+//nata
+else {
+ document.write ('<body style="background-color: #f0e5c9">');
+}
+
+</script>
+</head>
+<body>
+	<div id="wrapper" class="wrapper">
+		<header id="head">
+			<div class="wrapper1">
+				<div class="logo">
+					<a href="#">Hobi</a>
+				</div>
+			
+				    <div class="search">
+					<form action="post">
+						<input type="text" placeholder="Kërko në faqe">
+						<button type="submit"><i class="fas fa-search"></i></button>
+					</form>
+				    </div>
+				    <br><br><br>
+					<nav>
+		 			     <ul class="navigation">
+				  		 <li><a href="ballina.php">BALLINA</a></li>
+				  		 <li><a href="#">FULL WIDTH</a></li>
+							
+						<li id="dropdowni"><a href="#">KATEGORITË</a>
+							<ul>    
+								<li><a href="mode.php">Modë</a></li>
+								<li><a href="shendet.php">Shëndet</a></li>
+								<li><a href="hobi.php">Hobi</a></li>
+								<li><a href="storjet.php">Storje</a></li>
+							</ul>
+						</li>
+
+						<li><a href="loja.php">LOJA</a></li>
+						<li><a href="gallery.php">GALERIA</a></li>
+						
+						<li id="dropdowni"><a href="#">KYÇU/REGJISTROHU</a>
+							<ul>    
+								<li><a href="dragdrop.php">Kyçu</a></li>
+								<li><a href="regjistrimi.php">Regjistrohu</a></li>
+								<li><a href="contact.php">Na kontakto</a></li>
+							</ul>
+					   </li>						
+
+				  	     </ul>
+	      				</nav>
+				</div>	
+		 	</header>
+			
+		        			
+	         <main>
+			  		 
+								
+					
+				<section style="padding-top: 140px;" id="facilis">
+					<div class="facilis_title">
+						<h2 style="padding-top: 20px">Shumëçka nga hobi të ndryshëm </h2>
+						<div class="facilis_border"></div>
+					</div>
+						     
+					<div class="facilis_content"> 
+					     <ul>
+					        <li>
+								<div class="single_facilis">
+									<img src="img/udhetime.jpg" width="300" height="170" alt="img1">
+									<h4>UDHËTIME</h4>
+									<p><a href="https://www.youtube.com/watch?v=ZOJKZ0f5y38" target="_blank">
+										Niki Misureli shëtit botën me motor</a></p>
+									<p><a href="https://thesavvybackpacker.com/complete-guide-to-train-travel-in-europe/" target="_blank">
+									    Guidë e plotë për udhëtim me tren në Evropë</a></p>
+									<p><a href="https://travel.state.gov/content/travel/en/traveladvisories/traveladvisories/kosovo-travel-advisory.html" target="_blank">
+										Këshilla për udhëtim në Kosovë përgjatë pandemisë</a></p>
+						   		</div>
+					       </li>
+						     
+				            <li>
+								<div class="single_facilis">
+						  			<img src="img/hike.jpg" width="300" height="170" alt="img2">
+						  			<h4>HIKE</h4>
+						  			<p><a href="https://balkaneu.com/kosovo-conquers-everest/" target="_blank">
+										Nazmi Hasanramaj dhe Arineta Mula ngjiten në Everest</a></p>
+								    <p><a href="https://www.facebook.com/pg/KosovoMountainClub/events/?ref=page_internal" target="_blank">
+										Organizimet e Kosova Mountain Club</a></p>
+								    <p><a href="https://www.netflix.com/title/81004270" target="_blank">
+                                        Dokumentari "The down wall" për ngjitje të rrezikshme</a></p>
+					   			</div>
+							</li>
+						     
+							<li>
+								<div class="single_facilis">
+									<h4 style="padding-top: 30px;">MË SHUMË LAJME</h4>
+									<p><a href="https://www.youtube.com/watch?v=ZOJKZ0f5y38" target="_blank">
+										Niki Misureli shëtit botën me motor</a></p>
+									<p><a href="https://thesavvybackpacker.com/complete-guide-to-train-travel-in-europe/" target="_blank">
+									    Guidë e plotë për udhëtim me tren në Evropë</a></p>
+									<p><a href="https://travel.state.gov/content/travel/en/traveladvisories/traveladvisories/kosovo-travel-advisory.html" target="_blank">
+										Këshilla për udhëtim në Kosovë përgjatë pandemisë</a></p>
+									<p><a href="https://balkaneu.com/kosovo-conquers-everest/" target="_blank">
+											Nazmi Hasanramaj dhe Arineta Mula ngjiten në Everest</a></p>
+									<p><a href="https://www.facebook.com/pg/KosovoMountainClub/events/?ref=page_internal" target="_blank">
+											Organizimet e Kosova Mountain Club</a></p>
+									<p><a href="https://www.netflix.com/title/81004270" target="_blank">
+											Dokumentari "The down wall" për ngjitje të rrezikshme</a></p>
+																	
+								</div>
+							</li>
+			
+			
+							<li>
+					   			<div class="single_facilis">
+									<img src="img/fotografi.jpg" width="300" height="170" alt="img4">
+									<h4>FOTOGRAFI</h4>	
+									<p><a href="https://www.instagram.com/arben.llapashtica/?hl=en" target="_blank">
+										Arben Llapashtica ndan punën e tij në Instagram</a></p>
+									<p><a href="https://www.tailwindapp.com/blog/most-popular-pinterest-categories" target="_blank">
+										Kategoritë më të famshme të fotove në Pinterest</a></p>
+									<p><a href="https://www.nationalgeographic.com/photography/photo-tips/digital-photography-tips/" target="_blank">
+										Këshilla për foto me aparate digjitale</a></p>
+				       			</div>
+				  			</li>
+						   
+							<li>
+				   				<div class="single_facilis">
+									<img src="img/dekorime.jpg" width="300" height="170" alt="img5">
+									<h4>DEKORIME</h4>
+									<p><a href="https://www.housebeautiful.com/entertaining/holidays-celebrations/g25436531/new-years-eve-decorations/" target="_blank">
+										Trendet e dekorimeve për vitin 2021 </a></p>
+									<p><a href="https://www.elledecor.com/design-decorate/room-ideas/g596/36-things-bedroom-needs/" target="_blank">
+										Ide për dekore të vogla për dhomat e gjumit</a></p>
+									<p><a href="https://www.goodhousekeeping.com/home/gardening/advice/g1007/backyard-decorating/" target="_blank">
+										Zbukurime të lehta dhe të lezetshme për oborret</a></p>
+				    			</div>
+							</li>
+						   
+							<li>
+								<div class="single_facilis" style="padding-top: 15px;">
+									<h4>MË SHUMË NGA LAJME</h4>
+									<p><a href="https://www.instagram.com/arben.llapashtica/?hl=en" target="_blank">
+										Arben Llapashtica ndan punën e tij në Instagram</a></p>
+									<p><a href="https://www.tailwindapp.com/blog/most-popular-pinterest-categories" target="_blank">
+										Kategoritë më të famshme të fotove në Pinterest</a></p>
+									<p><a href="https://www.nationalgeographic.com/photography/photo-tips/digital-photography-tips/" target="_blank">
+										Këshilla për foto me aparate digjitale</a></p>
+									<p><a href="https://www.housebeautiful.com/entertaining/holidays-celebrations/g25436531/new-years-eve-decorations/" target="_blank">
+											Trendet e dekorimeve për vitin 2021 </a></p>
+									<p><a href="https://www.elledecor.com/design-decorate/room-ideas/g596/36-things-bedroom-needs/" target="_blank">
+											Ide për dekore të vogla për dhomat e gjumit</a></p>
+									<p><a href="https://www.goodhousekeeping.com/home/gardening/advice/g1007/backyard-decorating/" target="_blank">
+											Zbukurime të lehta dhe të lezetshme për oborret</a></p>	    							
+									
+								</div>
+							</li>
+						</ul>
+					</div>
+	             </section>
+									   
+			
+			<section id="footer_ContactUs">
+				<ul>
+					<li>
+						<div class="f_ContactUs">
+						    <h4>Na kontaktoni</h4>	
+							<address>
+						   
+						   Revista Lifestyle  <br>
+						   Rr. "Eqrem Çabej"<br>
+						   Prishtinë, Kosovë <br>
+						   10000 <br>
+						   Tel: +383 44 000 000 <br>
+						   Fax: +383 38 000 000 <br>
+						   E-mail: revistalifestyleks@gmail.com <br>
+						   
+							 </address>
+						   </div>
+					   </li>
+						   <li>
+					   <div class="f_ContactUs">
+						   <h4>Vegëza </h4>
+						   <ul>
+							   <li><a href="https://www.elle.com/">Revista Elle</a></li>
+							   <li><a href="http://lifestylemagazineonline.com/">Revista Lifestyle US</a></li> 
+							   <li><a href="https://www.theguardian.com/us/lifeandstyle">The Guardian / Lifestyle</a></li>
+							   <li><a href="https://www.lifestylesports.com/eu/">Lifestyle Sports</a></li>
+						   </ul>
+						   </div>
+					   </li>
+				   
+					   <li>
+						  <div class="f_ContactUs">
+						   <h4>Nga faqja </h4>
+						   <h3>Menaxhimi i faqes</h3>
+						   <p>
+							  Kjo faqe kontrollohet dhe menaxhohet nga stafi i Revistës Lifestyle. <br>
+							  Revista filloi si projekt gjatë studimeve në fakultet.
+							  </p>
+					   
+							   <div class="footer_read">
+							<a href="doc/per_faqen.pdf">Lexo më shumë >></a>
+							   </div>
+							  </div>
+						  </li>
+				   
+						   <li>
+						  <div class="f_ContactUs">
+						   <h4> Porositni revistën e shtypur </h4>
+						   <div class="footer_form">
+							   <form action="">
+								<input type="text" placeholder="Emri">
+							   <input type ="email" placeholder="E-mail-i">
+							   <input id="submitbutton" type="submit" value="SUBMIT">
+							   </form>
+							   </div>
+						   </div>
+					   </li>
+				   </ul>
+			   <ul id="icons" class="social-icons">
+					<li><a href="https://www.instagram.com/revistalifestyleks/"><i id="instagram" class="fab fa-instagram"></i></a></li>
+					<li><a href="https://www.facebook.com/reviste.lifstyl.7/"><i id="facebook" class="fab fa-facebook"></i></a></li>
+					<li><a href="https://twitter.com/RLifestyle_Ks"><i id="twitter" class="fab fa-twitter"></i></a></li>
+					</ul>
+					<br> <br>
+			   </section>
+		   
+		   <footer>
+				<div class="footer_text">
+					<p> Copyright © 2013-All Rights Reserved - Domain Name
+						<span> 
+							Template by OS Templates
+						</span>
+				    </p>
+			    </div>
+			</footer>
+		</main>
+	   </div>
+									   
+	</body>
+</html>
+                                                                                                                 
